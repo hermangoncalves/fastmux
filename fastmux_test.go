3a5b246f4347b968f99007937e1e117e02f17780
hermangoncalves <hermangoncalves@outlook.com>
2025-08-04 09:43:04 -0100

    Refactor TestRouter to TestRouterParam and add comprehensive unit tests for all HTTP method routes in Fastmux

--- fastmux_test.go
+++ fastmux_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestRouter(t *testing.T) {
+func TestRouterParam(t *testing.T) {
 	router := New()
 
 	router.Handle(http.MethodGet, "/hello/:name", func(ctx *Context) {
@@ -32,5 +32,83 @@ func TestRouter(t *testing.T) {
 
 	if w.Code != http.StatusOK {
 		t.Fatalf("unexpected status: got %d, want %d", w.Code, http.StatusOK)
+	}
+}
+
+func TestRoutes(t *testing.T) {
+	var get, head, options, post, put, patch, delete bool
+
+	router := New()
+
+	router.GET("/GET", func(ctx *Context) {
+		get = true
+	})
+
+	router.HEAD("/GET", func(ctx *Context) {
+		head = true
+	})
+
+	router.OPTIONS("/GET", func(ctx *Context) {
+		options = true
+	})
+
+	router.POST("/POST", func(ctx *Context) {
+		post = true
+	})
+
+	router.PUT("/PUT", func(ctx *Context) {
+		put = true
+	})
+
+	router.PATCH("/PATCH", func(ctx *Context) {
+		patch = true
+	})
+
+	router.DELETE("/DELETE", func(ctx *Context) {
+		delete = true
+	})
+
+	w := httptest.NewRecorder()
+
+	r, _ := http.NewRequest(http.MethodGet, "/GET", nil)
+	router.ServeHTTP(w, r)
+	if !get {
+		t.Error("routing GET failed")
+	}
+
+	r, _ = http.NewRequest(http.MethodHead, "/GET", nil)
+	router.ServeHTTP(w, r)
+	if !head {
+		t.Error("routing HEAD failed")
+	}
+
+	r, _ = http.NewRequest(http.MethodOptions, "/GET", nil)
+	router.ServeHTTP(w, r)
+	if !options {
+		t.Error("routing OPTIONS failed")
+	}
+
+	r, _ = http.NewRequest(http.MethodPost, "/POST", nil)
+	router.ServeHTTP(w, r)
+	if !post {
+		t.Error("routing POST failed")
+	}
+
+	r, _ = http.NewRequest(http.MethodPut, "/PUT", nil)
+	router.ServeHTTP(w, r)
+	if !put {
+		t.Error("routing PUT failed")
+	}
+
+	r, _ = http.NewRequest(http.MethodPatch, "/PATCH", nil)
+	router.ServeHTTP(w, r)
+	if !patch {
+		t.Error("routing PATCH failed")
+	}
+
+	r, _ = http.NewRequest(http.MethodDelete, "/DELETE", nil)
+	router.ServeHTTP(w, r)
+	if !delete {
+		t.Error("routing DELETE failed")
 	}
 }
